35a78a34c982e0d4835c9b82be141d009a2bad4d
Marco La Rosa <m@lr.id.au>
2023-01-18 10:04:54 +1100

    add element-plus

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+import "element-plus/theme-chalk/index.css";
 import "./assets/tailwind.css";
 import "@fortawesome/fontawesome-free/js/all";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoReplaceSvg = "nest";
+
+import ElementPlus from "element-plus";
 import { createApp } from "vue";
 import App from "./App.vue";
-
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(ElementPlus);
+app.mount("#app");
