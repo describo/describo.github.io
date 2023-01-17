9568ca0e23d4702c3ed7cbb3ea01aba9774cf539
Marco La Rosa <m@lr.id.au>
2023-01-17 21:56:53 +1100

    revise nav

--- src/components/Navigation.component.vue
+++ src/components/Navigation.component.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="flex flex-row space-x-8 border-b-2 p-8 text-3xl">
+    <div class="flex flex-row space-x-8 border-b-2 p-8 text-2xl">
         <div class="flex-grow"></div>
         <div>
             <a href="https://github.com/describo" target="_blank" class="hover:text-orange-600">
                 <i class="fa-brands fa-github"></i>
+                Describo Organisation
             </a>
         </div>
         <div>
@@ -12,7 +13,8 @@
                 target="_blank"
                 class="hover:text-orange-600"
             >
-                <i class="fa-solid fa-comment-dots"></i> Discussions
+                <i class="fa-solid fa-comment-dots"></i>
+                Discussions
             </a>
         </div>
         <div>
