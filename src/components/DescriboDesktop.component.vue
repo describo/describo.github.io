acfa6b14d359860e2055c952771f542a76982d08
Marco La Rosa <m@lr.id.au>
2023-05-20 19:03:19 +1000

    updates

--- src/components/DescriboDesktop.component.vue
+++ src/components/DescriboDesktop.component.vue
@@ -2,13 +2,22 @@
     <div class="flex flex-col space-y-20">
         <NavigationComponent />
 
-        <InfoPanelComponent heading="Describo Desktop" :image="desktop1">
+        <InfoPanelComponent :image="desktop1">
             <template #title>Describo Desktop</template>
             <template #text>
                 <div>
                     An application that you can use anywhere, anytime - even when you don't have any
                     network - to describe your data. It remember's what you were working on and
                     starts up next time right where you left off.
+                </div>
+            </template>
+        </InfoPanelComponent>
+
+        <InfoPanelComponent :image="desktop2">
+            <template #title>Automatic Updates</template>
+            <template #text>
+                <div>
+                    On startup, desktop checks for updates. If there is one, you'll be notified.
                 </div>
             </template>
         </InfoPanelComponent>
@@ -37,5 +46,6 @@ import NavigationComponent from "./Navigation.component.vue";
 import InfoPanelComponent from "./InfoPanel.component.vue";
 import RepositoryLinksComponent from "./RepositoryLinks.component.vue";
 import desktop1 from "../assets/describo-1.png";
+import desktop2 from "../assets/desktop-1.png";
 import ReleasesComponent from "./Releases.component.vue";
 </script>
