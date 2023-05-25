87e671108e48749f6f843c80bc5af5755669133d
Marco La Rosa <m@lr.id.au>
2023-05-25 20:33:33 +1000

    add logo to navigation; update styling

--- src/components/Navigation.component.vue
+++ src/components/Navigation.component.vue
@@ -1,31 +1,38 @@
 <template>
     <div class="sticky top-0 z-50">
-        <div class="flex flex-row justify-around md:space-x-8 p-4 text-xs md:text-2xl bg-slate-200">
-            <router-link to="/"><i class="fa-solid fa-house"></i>&nbsp;describo</router-link>
-            <div class="flex-grow hidden md:inline"></div>
+        <div class="flex flex-row justify-between md:space-x-8 p-2 text-2xl bg-slate-200">
+            <router-link to="/">
+                <img :src="logo" class="h-10 md:h-20" />
+            </router-link>
+            <div class="flex-grow md:inline"></div>
             <div>
-                <a href="https://github.com/describo" target="_blank" class="hover:text-orange-600">
+                <a
+                    href="https://github.com/describo"
+                    target="_blank"
+                    class="m-2 hover:text-orange-600"
+                >
                     <i class="fa-brands fa-github"></i>
-                    Organisation
+                    <div class="hidden md:inline">&nbsp;Organisation</div>
                 </a>
             </div>
             <div>
                 <a
                     href="https://github.com/orgs/describo/discussions"
                     target="_blank"
-                    class="hover:text-orange-600"
+                    class="m-2 hover:text-orange-600"
                 >
                     <i class="fa-solid fa-comment-dots"></i>
-                    Discussions
+                    <div class="hidden lg:inline">&nbsp;Discussions</div>
                 </a>
             </div>
             <div>
                 <a
                     href="https://github.com/orgs/describo/discussions/categories/announcements"
                     target="_blank"
-                    class="hover:text-orange-600"
+                    class="m-2 hover:text-orange-600"
                 >
-                    <i class="fa-solid fa-bullhorn"></i> Announcements
+                    <i class="fa-solid fa-bullhorn"></i>
+                    <div class="hidden lg:inline">&nbsp;Announcements</div>
                 </a>
             </div>
         </div>
@@ -33,6 +40,5 @@
 </template>
 
 <script setup>
-import { useRoute } from "vue-router";
-const $route = useRoute();
+import logo from "../assets/describo-logo10-trans.png";
 </script>
