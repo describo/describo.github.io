b3266b1a2b8a71c740174814feab7fadabf07d6e
Marco La Rosa <m@lr.id.au>
2023-12-21 14:38:32 +1100

    update site

--- docs/assets/Dashboard-1f7aca49.js
+++ docs/assets/Dashboard-1f7aca49.js
@@ -1,3 +1,3 @@
-import{o,c as t,_ as a}from"./index-cda1d53a.js";const i={__name:"Dashboard",setup(s){const e=[{text:`When you first start Describo desktop, the main dashboard will be shown. From here
+import{o,c as t,_ as a}from"./index-9f4d2f28.js";const i={__name:"Dashboard",setup(s){const e=[{text:`When you first start Describo desktop, the main dashboard will be shown. From here
         you can select a folder of content to describe or load a previous used folder. The main application
         settings are also accessible with the button in the top right of the navigation.`,image:"/documentation/desktop/dashboard1.png"}];return(r,n)=>(o(),t(a,{cards:e,class:"bg-slate-100 p-4 rounded-lg"}))}};export{i as default};
