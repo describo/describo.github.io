3c6d3c9e233704935d1ead8e1e6b8f5fa6591bee
Marco La Rosa <m@lr.id.au>
2023-12-23 09:32:38 +1100

    content update

--- src/components/Footer.component.vue
+++ src/components/Footer.component.vue
@@ -1,23 +1,25 @@
 <template>
-    <div class="flex flex-row my-10 border-t border-slate-400">
-        <div class="flex flex-col w-96">
-            <div>
-                <router-link to="/">
-                    <img src="/logos/describo-logo.png" class="h-16 md:h-32" />
-                </router-link>
+    <div class="flex flex-col md:flex-row place-content-between my-10 border-t border-slate-400">
+        <div class="flex flex-col items-center md:items-start w-96">
+            <router-link to="/">
+                <img src="/logos/describo-logo.png" class="h-16 lg:h-24" />
+            </router-link>
+        </div>
+        <div class="flex flex-col md:flex-row place-content-evenly">
+            <div class="flex flex-col md:w-48">
+                <div><router-link to="/desktop"> Desktop</router-link></div>
+                <div><router-link to="/web"> Web</router-link></div>
+                <div><router-link to="/component"> Component</router-link></div>
+                <div><router-link to="/profile"> Profiles</router-link></div>
+                <div><router-link to="/datapacks"> Data Packs</router-link></div>
             </div>
-        </div>
-        <div class="flex flex-col w-96">
-            <div><router-link to="/desktop"> Desktop</router-link></div>
-            <div><router-link to="/web"> Web</router-link></div>
-            <div><router-link to="/component"> Component</router-link></div>
-            <div><router-link to="/profile"> Profiles</router-link></div>
-            <div><router-link to="/datapacks"> Data Packs</router-link></div>
-        </div>
-        <div class="flex flex-col">
-            <div><router-link to="/about"> About Describo</router-link></div>
-            <!-- <div><router-link to="/faqs"> FAQs</router-link></div> -->
-            <div><router-link to="/documentation"> Documentation and Tutorials</router-link></div>
+            <div class="flex flex-col md:w-60">
+                <div><router-link to="/about"> About Describo</router-link></div>
+                <!-- <div><router-link to="/faqs"> FAQs</router-link></div> -->
+                <div>
+                    <router-link to="/documentation"> Documentation and Tutorials</router-link>
+                </div>
+            </div>
         </div>
     </div>
 </template>
