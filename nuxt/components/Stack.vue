bf484e74d862d0978d906f676631023088e5fadb
Marco La Rosa <m@lr.id.au>
2024-01-10 17:41:01 +1100

    revise as nuxt app

---
+++ nuxt/components/Stack.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="flex flex-col space-y-2 text-slate-800" v-cloak>
+        <div class="flex flex-row items-center text-sm xl:text-lg flex-grow">
+            <div
+                class="hidden md:flex cursor-pointer text-4xl py-48 px-2"
+                @click="previousCard()"
+                v-if="cards.length > 1"
+            >
+                <font-awesome-icon icon="fa-solid fa-chevron-left" />
+            </div>
+            <div v-auto-animate="{ duration: 300 }">
+                <card :key="ref(idx)" v-for="(card, idx) of cards" :class="{ hidden: idx !== n }">
+                    <template #text><div v-html="card.text"></div></template>
+                    <template #image>
+                        <div class="flex flex-col space-y-2">
+                            <img
+                                v-if="!isArray(card.image)"
+                                :src="card.image"
+                                class="object-contain"
+                            />
+                            <img
+                                v-else
+                                :src="image"
+                                v-for="image of card.image"
+                                class="object-contain"
+                            />
+                        </div>
+                    </template>
+                </card>
+            </div>
+            <div
+                class="hidden md:flex cursor-pointer text-4xl py-48 px-2"
+                @click="nextCard()"
+                v-if="cards.length > 1"
+            >
+                <i class="fa-solid fa-chevron-right"></i>
+                <font-awesome-icon icon="fa-solid fa-chevron-right" />
+            </div>
+        </div>
+        <div class="hidden md:flex flex-row place-content-end">
+            {{ n + 1 }} / {{ cards.length }}
+        </div>
+        <div class="flex flex-row md:hidden place-content-between" v-if="cards.length > 1">
+            <div class="cursor-pointer px-2" @click="previousCard()">
+                <font-awesome-icon icon="fa-solid fa-chevron-left" />
+            </div>
+            <div>{{ n + 1 }} / {{ cards.length }}</div>
+            <div class="cursor-pointer px-2" @click="nextCard()">
+                <font-awesome-icon icon="fa-solid fa-chevron-right" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { vAutoAnimate } from "@formkit/auto-animate";
+import Card from "./Card.vue";
+import { ref } from "vue";
+import isArray from "lodash-es/isArray.js";
+
+const props = defineProps({
+    cards: { type: Array },
+});
+const n = ref(0);
+
+function nextCard() {
+    n.value = n.value < props.cards.length - 1 ? (n.value += 1) : 0;
+}
+function previousCard() {
+    n.value = n.value === 0 ? props.cards.length - 1 : (n.value -= 1);
+}
+</script>
---
+++ nuxt/nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    devtools: { enabled: false },
+    modules: ["@nuxtjs/tailwindcss"],
+    css: ["@fortawesome/fontawesome-svg-core/styles.css"],
+    build: {
+        transpile: [
+            "@fortawesome/vue-fontawesome",
+            "@fortawesome/fontawesome-svg-core",
+            "@fortawesome/free-solid-svg-icons",
+            "@fortawesome/free-regular-svg-icons",
+            "@fortawesome/free-brands-svg-icons",
+        ],
+    },
+    app: {
+        head: {
+            bodyAttrs: {
+                class: "bg-slate-200",
+            },
+        },
+    },
+});
