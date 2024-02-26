8abc7f2777bcdc1963d19e6e5633c596258ebc1b
Marco La Rosa <m@lr.id.au>
2024-02-26 12:42:01 +1100

    update tour

--- pages/index.vue
+++ pages/index.vue
@@ -124,65 +124,132 @@ import LinkComponent from "../components/Link.component.vue";
 
 const cards = [
     {
-        text: "The dashboard. Select a folder of content to describe; load a previously used folder or manage data pack data.",
+        text: `The dashboard. Select a folder of content to describe; load a previously used folder or manage data pack data.
+        Whether you are describing research data or creating a cultural collection, eveyrthing you do will be stored in this folder.
+        Nothing is locked up inside Describo.`,
         image: "/desktop1.png",
     },
+
     {
-        text: "Context aware help.",
-        image: "/desktop1a.png",
-    },
-    {
-        text: `When the RO Crate file is loaded the root dataset is shown on the right along with a file browser on the left.
-       In the navigation bar we can see which folder is loaded and controls to load a profile as well as access the tool settings.
+        text: `When the RO Crate file is loaded the root dataset is shown in the middle pane along with a file browser on the left.
+       In the navigation bar we can see which folder is loaded as well as controls to load a profile and access application settings.
         `,
         image: "/desktop2.png",
     },
     {
-        text: `Selecting a entity of type 'File' will display a preview of that file. The preview can be full screened to get a better look at it.`,
+        text: "Pressing the help button activates context setting help.",
+        image: "/desktop1a.png",
+    },
+    {
+        text: `Selecting a entity of type 'File' will calculate file metadata, create an entry for the file in the crate and display a preview of it
+        in the right hand panel. The preview can be full screened to get a better look at the file.`,
         image: "/desktop3.png",
     },
     {
         text: `Describo makes all of Schema.org available to you. In the image we can see all of the properties defined by schema.org
-        for an entity of type Dataset; all the way back up the hierarchy to Thing. In addition, we can see what data types each property is expecting.
-        The 'about' property expects an entity of type 'Thing' and describo will enforce that.`,
-        image: "desktop4.png",
+        for an entity of type File; all the way back up the hierarchy to Thing. In addition, we can see what data types each property is expecting.
+        The 'about' property expects an entity of type 'Thing' and Describo will enforce that.`,
+        image: "/desktop4.png",
     },
     {
-        text: `Entities can be created directly. 'Spatial Coverge' is expecting an entity of type place and describo provides controls
-        to create a new entity. In this case, we are creating an entity of type place with the name 'Somewhere'.`,
-        image: "desktop5.png",
+        text: `Entities can be created directly. 'Spatial Coverage' is expecting an entity of type Place and Describo provides controls
+        to create a new entity of that type. In this example, we are creating an entity of type Place with the name 'Somewhere'.`,
+        image: "/desktop5.png",
     },
     {
         text: `Navigating to the entity we can then start managing its data. Shown is the Geometry component which enables creating
         a custom geometry for the Geo property.`,
-        image: "desktop6.png",
+        image: "/desktop6.png",
     },
     {
         text: `Describo is totally configurable via profiles. Profiles are JSON files that describe how the interface looks and  what a user can do.
         Profiles can be loaded from your computer or from the Describo Profiles Repository. Profiles are cached locally for reuse.`,
-        image: "desktop7.png",
+        image: "/desktop7.png",
     },
     {
-        text: `When a profile is loaded, the view will adapt based on the content of that profile. In this case, the profile defines
+        text: `When a profile is loaded, the view will adapt based on the content of that profile. In this example, the profile defines
         a tabbed layout for Dataset entities in addition to showing specific properties. This allows the profile author to define
         exactly how the UI should look for their users and guide them on what is required.`,
-        image: "desktop8.png",
+        image: "/desktop8.png",
     },
     {
-        text: `Describo is configurable. It supports English and Hungarian (for now) and there are controls for various aspects of its operation.`,
-        image: ["desktop9.png", "describo-i18-en.png", "describo-i18-hu.png"],
+        text: `Describo is totally configurable. It supports English and Hungarian (for now) and there are controls for various aspects of its operation.`,
+        image: ["/desktop9.png", "/describo-i18-en.png", "/describo-i18-hu.png"],
     },
     {
-        text: `There are controls to view the entities in the crate and jump directly to any entity wherever it is.`,
-        image: "desktop10.png",
+        text: `There are controls to view the entities in the crate and jump directly to any entity.`,
+        image: "/desktop10.png",
     },
-    { text: `There is also a control to directly edit the context.`, image: "desktop11.png" },
-    { text: `And one to preview the JSON linked data.`, image: "desktop12.png" },
-    { text: `Describo is themable.`, image: "desktop13.png" },
-    { text: `Describo can display crates in readonly mode.`, image: "desktop14.png" },
+    { text: `There is a control to directly edit the context.`, image: "/desktop11.png" },
+    { text: `And one to preview the JSON linked data.`, image: "/desktop12.png" },
+    { text: `Describo is themable.`, image: "/desktop13.png" },
+    { text: `Describo can display crates in readonly mode.`, image: "/desktop14.png" },
     {
         text: `Describo can display complex entities like actions and relationships that link other entities together.`,
-        image: "desktop15.png",
+        image: "/desktop15.png",
     },
 ];
+// const cards = [
+//     {
+//         text: "The dashboard. Select a folder of content to describe; load a previously used folder or manage data pack data.",
+//         image: "/desktop1.png",
+//     },
+//     {
+//         text: "Context aware help.",
+//         image: "/desktop1a.png",
+//     },
+//     {
+//         text: `When the RO Crate file is loaded the root dataset is shown on the right along with a file browser on the left.
+//        In the navigation bar we can see which folder is loaded and controls to load a profile as well as access the tool settings.
+//         `,
+//         image: "/desktop2.png",
+//     },
+//     {
+//         text: `Selecting a entity of type 'File' will display a preview of that file. The preview can be full screened to get a better look at it.`,
+//         image: "/desktop3.png",
+//     },
+//     {
+//         text: `Describo makes all of Schema.org available to you. In the image we can see all of the properties defined by schema.org
+//         for an entity of type Dataset; all the way back up the hierarchy to Thing. In addition, we can see what data types each property is expecting.
+//         The 'about' property expects an entity of type 'Thing' and describo will enforce that.`,
+//         image: "desktop4.png",
+//     },
+//     {
+//         text: `Entities can be created directly. 'Spatial Coverge' is expecting an entity of type place and describo provides controls
+//         to create a new entity. In this case, we are creating an entity of type place with the name 'Somewhere'.`,
+//         image: "desktop5.png",
+//     },
+//     {
+//         text: `Navigating to the entity we can then start managing its data. Shown is the Geometry component which enables creating
+//         a custom geometry for the Geo property.`,
+//         image: "desktop6.png",
+//     },
+//     {
+//         text: `Describo is totally configurable via profiles. Profiles are JSON files that describe how the interface looks and  what a user can do.
+//         Profiles can be loaded from your computer or from the Describo Profiles Repository. Profiles are cached locally for reuse.`,
+//         image: "desktop7.png",
+//     },
+//     {
+//         text: `When a profile is loaded, the view will adapt based on the content of that profile. In this case, the profile defines
+//         a tabbed layout for Dataset entities in addition to showing specific properties. This allows the profile author to define
+//         exactly how the UI should look for their users and guide them on what is required.`,
+//         image: "desktop8.png",
+//     },
+//     {
+//         text: `Describo is configurable. It supports English and Hungarian (for now) and there are controls for various aspects of its operation.`,
+//         image: ["desktop9.png", "describo-i18-en.png", "describo-i18-hu.png"],
+//     },
+//     {
+//         text: `There are controls to view the entities in the crate and jump directly to any entity wherever it is.`,
+//         image: "desktop10.png",
+//     },
+//     { text: `There is also a control to directly edit the context.`, image: "desktop11.png" },
+//     { text: `And one to preview the JSON linked data.`, image: "desktop12.png" },
+//     { text: `Describo is themable.`, image: "desktop13.png" },
+//     { text: `Describo can display crates in readonly mode.`, image: "desktop14.png" },
+//     {
+//         text: `Describo can display complex entities like actions and relationships that link other entities together.`,
+//         image: "desktop15.png",
+//     },
+// ];
 </script>
