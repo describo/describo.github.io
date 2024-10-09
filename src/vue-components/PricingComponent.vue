f81cd9db01b1f577352d12aed5c2c52b3dd44ca3
Marco La Rosa <m@lr.id.au>
2024-10-09 15:01:09 +1100

    add buttons to buy vocab credits and be a supporter

--- src/vue-components/PricingComponent.vue
+++ src/vue-components/PricingComponent.vue
@@ -5,6 +5,41 @@
 
         <!-- pricing pills and purchase controls -->
         <div class="flex flex-col space-y-2">
+            <Product class="w-full">
+                <template #title>Support Product Development</template>
+                <template #price>$25 USD</template>
+                <template #content>
+                    <p>
+                        Support continued development of the product. You don't have to but it's
+                        much appreciated if you do!
+                    </p>
+                    <p>
+                        You'll also get $5 in General Credits and 2 months of access to the
+                        Vocabulary creation tool so that you can try all of the capabilities.
+                    </p>
+                </template>
+                <template #controls v-if="!props.displayOnly">
+                    <stripe-buy-button
+                        :buy-button-id="pricing[environment].support.buttonId"
+                        :publishable-key="token"
+                    >
+                    </stripe-buy-button>
+                </template>
+            </Product>
+            <Product>
+                <template #title>Vocabulary Credits</template>
+                <template #price>$15 USD</template>
+                <template #content>
+                    Enable the Vocabulary creation tool for 6 months from the date or purchase.
+                </template>
+                <template #controls v-if="!props.displayOnly">
+                    <stripe-buy-button
+                        :buy-button-id="pricing[environment].vocab.buttonId"
+                        :publishable-key="token"
+                    >
+                    </stripe-buy-button>
+                </template>
+            </Product>
             <div class="flex flex-row space-x-2">
                 <Product class="w-1/2">
                     <template #title>General Credits</template>
@@ -12,9 +47,9 @@
                     <template #content>
                         <ul class="pl-10 list-disc">
                             <li>Text extraction credits for 50 pages</li>
-                            <li>Enough credits to perform entity recognition on 50 pages</li>
+                            <li>Enough credits to perform Named Entity Recognition on 50 pages</li>
                             <li>
-                                Credits to use the assistant to interrogate the data<sup>*</sup>
+                                Credits to use the Assistant to interrogate the data<sup>*</sup>
                                 <ul class="pl-4 list-disc">
                                     <li>2,000,000 input tokens</li>
                                     <li>16,000 output tokens</li>
@@ -36,7 +71,7 @@
                     <template #content>
                         <ul class="pl-10 list-disc">
                             <li>
-                                Credits to use the assistant to interrogate the data<sup>*</sup>
+                                Credits to use the Assistant to interrogate the data<sup>*</sup>
                                 <ul class="pl-4 list-disc">
                                     <li>4,000,000 input tokens</li>
                                     <li>40,000 output tokens</li>
@@ -55,7 +90,7 @@
             </div>
             <div class="flex flex-row space-x-2">
                 <Product class="w-1/2">
-                    <template #title>Entity Recognition Credits</template>
+                    <template #title>Named Entity Recognition Credits</template>
                     <template #price>$5 USD</template>
                     <template #content>
                         <ul class="pl-4 list-disc">
@@ -103,8 +138,6 @@
 <script setup>
 import Terms from "./Terms.vue";
 import Product from "./Product.vue";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faCircleXmark, faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { onBeforeMount, ref } from "vue";
 
 const props = defineProps({
@@ -126,6 +159,12 @@ const pricing = {
         entityRecognition: {
             buttonId: "buy_btn_1PzUmI01z7UukHJYScCBlX27",
         },
+        vocab: {
+            buttonId: "buy_btn_1Q7pNv01z7UukHJY3NR0u3eZ",
+        },
+        support: {
+            buttonId: "buy_btn_1Q7pZv01z7UukHJYsqYwpKED",
+        },
     },
     production: {
         general: {
@@ -139,6 +178,12 @@ const pricing = {
         },
         entityRecognition: {
             buttonId: "buy_btn_1PzXXl01z7UukHJYkXjFb525",
+        },
+        vocab: {
+            buttonId: "buy_btn_1Q7pel01z7UukHJYyt5t2AzS",
+        },
+        support: {
+            buttonId: "buy_btn_1Q7pg401z7UukHJYww41ax8d",
         },
     },
 };
