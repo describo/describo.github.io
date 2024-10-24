ae54480a0252e07afba909c3a6189e867ffd4ede
Marco La Rosa <m@lr.id.au>
2024-10-24 11:33:09 +1100

    styling

--- src/vue-components/DescriboFeatures.vue
+++ src/vue-components/DescriboFeatures.vue
@@ -3,31 +3,31 @@
         <FeatureComponent
             link="/desktop"
             :icon="faDownload"
-            class="col-span-1 md:col-span-2 lg:col-span-1 r bg-blue-200"
+            class="col-span-1 md:col-span-2 lg:col-span-1 r bg-blue-100"
         >
             <template #title> Get Describo - free download </template>
         </FeatureComponent>
-        <FeatureComponent link="/tour" :icon="faRoute" class="col-span-1 bg-blue-200">
+        <FeatureComponent link="/tour" :icon="faRoute" class="col-span-1 bg-blue-100">
             <template #title> Take the product tour </template>
         </FeatureComponent>
         <FeatureComponent
             link="/docs/articles/why-use-it"
-            :icon="faFileLines"
-            class="col-span-1 bg-blue-200"
+            :icon="faInfoCircle"
+            class="col-span-1 bg-blue-100"
         >
             <template #title> Why Describo? </template>
         </FeatureComponent>
         <FeatureComponent
             link="/docs/articles/articles-index"
             :icon="faFileLines"
-            class="col-span-1 md:col-span-2 lg:col-span-1 bg-blue-200"
+            class="col-span-1 md:col-span-2 lg:col-span-1 bg-blue-100"
         >
             <template #title> Feature Articles </template>
         </FeatureComponent>
         <FeatureComponent
             link="/docs/guide/five-minute-tutorial"
             :icon="faPersonChalkboard"
-            class="col-span-1 lg:col-span-2 bg-slate-200"
+            class="col-span-1 lg:col-span-2 bg-slate-100"
         >
             <template #title> 5 minute beginner tutorial </template>
             <template #content>
@@ -37,27 +37,27 @@
         <FeatureComponent
             link="/docs/component/introduction"
             :icon="faCode"
-            class="col-span-1lg:col-span-1 bg-slate-200"
+            class="col-span-1lg:col-span-1 bg-slate-100"
         >
             <template #title> Developers </template>
             <template #content>
                 <div class="text-xs no-underline">Use the Describo RO-Crate engine in your app</div>
             </template>
         </FeatureComponent>
-        <FeatureComponent link="/describo-users" :icon="faUsers" class="bg-slate-200">
+        <FeatureComponent link="/describo-users" :icon="faUsers" class="bg-slate-100">
             <template #title> See who's using Describo </template>
         </FeatureComponent>
         <FeatureComponent
             link="https://github.com/describo/describo.github.io"
             target="_blank"
             :icon="faStar"
-            class="col-span-1lg:col-span-1 bg-slate-200"
+            class="col-span-1lg:col-span-1 bg-slate-100"
         >
             <template #title> Like it? Star us on Github. </template>
         </FeatureComponent>
         <FeatureComponent
             :icon="faBookOpenReader"
-            class="col-span-1 md:col-span-2 lg:col-span-3 lg:row-span-1 bg-slate-200"
+            class="col-span-1 md:col-span-2 lg:col-span-3 lg:row-span-1 bg-slate-100"
         >
             <template #title> Cite Describo </template>
             <template #content>
@@ -82,5 +82,7 @@ import {
     faBook,
     faFileLines,
     faWandSparkles,
+    faQuestionCircle,
+    faInfoCircle,
 } from "@fortawesome/free-solid-svg-icons";
 </script>
--- src/vue-components/Feature.vue
+++ src/vue-components/Feature.vue
@@ -3,7 +3,7 @@
         class="flex flex-row space-x-3 md:place-content-center items-center py-4 px-4 lg:p-4 rounded-lg"
     >
         <div class="text-slate-700" v-if="props.icon">
-            <font-awesome-icon :icon="props.icon" :size="props.size" />
+            <font-awesome-icon :icon="props.icon" :size="props.size" class="text-indigo-600" />
         </div>
         <div class="text-sm md:text-base flex flex-col space-y-2 place-content-center">
             <LinkComponent :link="props.link" :target="props.target" v-if="props.link">
